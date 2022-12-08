f3e1abdaa11a20f99ed4cd90fec27dad5201c3e8
Xevnar <Xevnar@gmail.com>
2022-12-08 14:24:21 +0300

    chore(ci): Remove tests we actively don't want to pass

--- .ci/run_test_filetype.sh
+++ .ci/run_test_filetype.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/sh
 
 cp -vr ./lua ./filetype.lua ./neovim/runtime
+
+# Delete shebang tests that we don't care about passing from `Test_script_detection`
+sed -i -E -e '\%.*#!/path/(bash|dash|ksh).*%d' -e "s%\[\['#!/path/sh'],%[['#!/path/sh']],%g" neovim/src/nvim/testdir/test_filetype.vim
+
+# Delete test from `Test_sig_file` that we don't care about
+sed -i -E -e '1807,1810d' neovim/src/nvim/testdir/test_filetype.vim
+
 cd neovim
 make oldtest TEST_FILE=test_filetype.vim
