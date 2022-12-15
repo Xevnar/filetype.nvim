9e07094c4e396baedb007807038fc68c922de014
Xevnar <Xevnar@gmail.com>
2022-12-16 00:41:08 +0300

    feat(ci): add second test inspecting complex maps

--- .ci/run_test_filetype.sh
+++ .ci/run_test_filetype.sh
@@ -11,3 +11,11 @@ sed -i -E -e '5,243d' neovim/src/nvim/testdir/test_filetype.vim
 
 cd neovim
 TEST_FILE='test_filetype.vim' make oldtest
+cat ./src/nvim/testdir/filetype.nvim.log
+
+# Rename log file
+sed -i -e 's#local outfile = string.format('\''./%s.log'\'', config.plugin)#local outfile = string.format('\''./%s_2.log'\'', config.plugin)#g' ./runtime/lua/vlog/init.lua
+sed -i -E -e '296i logger.trace('\''Complex_maps: '\'', vim.inspect(complex_maps))' ./runtime/lua/filetype/init.lua
+
+TEST_FILE='test_filetype.vim' make oldtest
+cat ./src/nvim/testdir/filetype.nvim_2.log
