500fba39153826106827f82e006c099bc9cfdd4e
Xevnar <Xevnar@gmail.com>
2022-12-24 16:02:21 +0300

    neovim: conflict between supercollider and scala filetype detection (https://github.com/neovim/neovim/pull/21417)
    Problem:    Conflict between supercollider and scala filetype detection.
    Solution:   Do not check for "Class : Method", it can appear in both
                filetypes. (Chris Kipp, closes vim/vim#11699)
    
    https://github.com/vim/vim/commit/70ef3f5
    https://github.com/neovim/neovim/commit/4a78ed
    
    Co-authored-by: Chris Kipp <ckipp@pm.me>

--- lua/filetype/detect.lua
+++ lua/filetype/detect.lua
@@ -1156,17 +1156,7 @@ end
 --- @return string? # The detected filetype
 function M.sc()
 	for _, line in ipairs(util.getlines(0, M.line_limit)) do
-		if
-			util.findany(line, {
-				'[A-Za-z0-9]*%s:%s[A-Za-z0-9]',
-				'var%s<',
-				'classvar%s<',
-				'%^this.*',
-				'|%w*|',
-				'%+%s%w*%s{',
-				'%*ar%s',
-			})
-		then
+		if util.findany(line, { 'var%s<', 'classvar%s<', '%^this.*', '|%w+|', '%+%s%w*%s{', '%*ar%s', }) then
 			return 'supercollider'
 		end
 	end
