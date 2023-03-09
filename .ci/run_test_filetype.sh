73bbd7da4c8c2c4da0502e65fba44dcb7ae9967b
Xevnar <Xevnar@gmail.com>
2023-03-09 18:39:11 +0300

    chore(ci): updated the path of the old tests in neovim

--- .ci/run_test_filetype.sh
+++ .ci/run_test_filetype.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/sh
 
+# Copy the repo to neovim overrwiting it's defaults
 cp -vr ./lua ./filetype.lua ./neovim/runtime
 
+# The location of the old test file
+TEST_FILE='neovim/test/old/testdir/test_filetype.vim'
+
 # Delete test from `Test_sig_file` that we don't care about
-sed -i -E -e '1859d' neovim/src/nvim/testdir/test_filetype.vim
+sed -i -E -e '1859d' "$TEST_FILE"
 
 # Delete the `Test_conf_file` test.
-sed -i -E -e '15,27d' neovim/src/nvim/testdir/test_filetype.vim
+sed -i -E -e '15,27d' "$TEST_FILE"
 
 # Delet bash test from `Test_filetype_detection`
-sed -i -E -e "s/'\.bashrc', 'file\.bash', //g" neovim/src/nvim/testdir/test_filetype.vim
+sed -i -E -e "s/'\.bashrc', 'file\.bash', //g" "$TEST_FILE"
 
 # Delete shebang tests that we don't care about passing from `Test_script_detection`
-sed -i -E -e '\%.*#!/path/(bash|dash|ksh).*%d' -e "s%\[\['#!/path/sh'],%[['#!/path/sh']],%g" neovim/src/nvim/testdir/test_filetype.vim
+sed -i -E -e '\%.*#!/path/(bash|dash|ksh).*%d' -e "s%\[\['#!/path/sh'],%[['#!/path/sh']],%g" "$TEST_FILE"
 
 cd neovim
 TEST_FILE='test_filetype.vim' make oldtest
